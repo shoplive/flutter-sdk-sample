1157c7dcd69b39a6e9771b430d1d319387e4b6f3
Loco <loco@shoplive.cloud>
2024-12-11 16:44:54 +0900

    Bump version to v1.6.2 (#18)

--- shoplive_flutter_sample/android/src/main/kotlin/cloud/shoplive/shoplive_player/ShopliveBaseModule.kt
+++ shoplive_flutter_sample/android/src/main/kotlin/cloud/shoplive/shoplive_player/ShopliveBaseModule.kt
@@ -11,7 +11,6 @@ import io.flutter.plugin.common.BinaryMessenger
 import io.flutter.plugin.common.EventChannel
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
-import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import org.json.JSONObject
 import java.lang.reflect.Type
